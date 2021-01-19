844fca1ba248dfbb839957bd5f8b6c1702923b0f
Artem <nwlf.420@gmail.com>
2021-01-19 17:56:59 +0300

    Some work with custom cell

--- MyPlaces/MainTableView.swift
+++ MyPlaces/MainTableView.swift
@@ -28,13 +28,14 @@ class MainTableView: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomCell
 
-        cell.textLabel?.text = restaraunts[indexPath.row]
-        cell.imageView?.image = UIImage(named: restaraunts[indexPath.row])
+        cell.nameLabel.text = restaraunts[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: restaraunts[indexPath.row])
         
-        cell.imageView?.layer.cornerRadius = cell.frame.height / 2 
-        cell.imageView?.clipsToBounds = true
+        // Делаем изображение круглым
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
+        cell.imageOfPlace.clipsToBounds = true
 
         return cell
     }
